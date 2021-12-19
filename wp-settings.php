40b76e2a93c54c083b2dcd4454cda346a63e0565
Keisuke Kuzukawa <kuzukawa@gmail.com>
2021-12-19 15:01:00 +0900

    add condition to not using ssl if running on localhost

--- wp-settings.php
+++ wp-settings.php
@@ -16,10 +16,12 @@
 define( 'WPINC', 'wp-includes' );
 
 // Add for CloudFront(https) to ALB(http) connection.
-define( 'FORCE_SSL_ADMIN', true );
-if ( ! empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
-       $_SERVER['HTTPS']='on';
-       $_ENV['HTTPS']='on';
+if (@$_SERVER["SERVER_NAME"] !== 'localhost') {
+	define( 'FORCE_SSL_ADMIN', true );
+	if ( ! empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) {
+	       $_SERVER['HTTPS']='on';
+    	   $_ENV['HTTPS']='on';
+	}
 }
 
 /**
